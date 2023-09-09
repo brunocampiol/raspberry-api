cdb675f3ae19499c110f6425aacd55bd3a2f6b17
Bruno Paes <bruno.campiol@gmail.com>
2023-09-09 13:40:59 -0300

    update settings controller and swagger routing for prod

--- src/RaspberryPi.API/Controllers/SettingsController.cs
+++ src/RaspberryPi.API/Controllers/SettingsController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 namespace RaspberryPi.API.Controllers
 {
@@ -6,30 +9,37 @@ namespace RaspberryPi.API.Controllers
     [Route("[controller]")]
     public class SettingsController : ControllerBase
     {
-        private readonly IConfiguration _configuration;
         private readonly IWebHostEnvironment _hostEnv;
 
-        public SettingsController(IConfiguration configuration, IWebHostEnvironment hostEnv)
+        public SettingsController(IWebHostEnvironment hostEnv)
         {
-            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
             _hostEnv = hostEnv ?? throw new ArgumentNullException(nameof(hostEnv));
         }
-
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            var configuration = _configuration.AsEnumerable();
+            string? frameworkName = Assembly.GetEntryAssembly()?.GetCustomAttribute<TargetFrameworkAttribute>()?.FrameworkName;
+            string? aspNetCoreEnv = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            string osDescription = $"{RuntimeInformation.OSDescription} [ {RuntimeInformation.OSArchitecture} ]";
 
             var settings = new
             {
                 EnvironmentName = _hostEnv.EnvironmentName,
+                ASPNetCore_Environment = aspNetCoreEnv,
+                OSDescription = osDescription,
+                FrameworkName = frameworkName,
                 IsDevelopment = _hostEnv.IsDevelopment(),
                 IsProduction = _hostEnv.IsProduction(),
-                Configuration = configuration
             };
 
             return Ok(settings);
+        }
+
+        [HttpGet("datetime")]
+        public async Task<IActionResult> DateAndTime()
+        {
+            return Ok(DateTime.Now);
         }
     }
 }
--- src/RaspberryPi.API/Program.cs
+++ src/RaspberryPi.API/Program.cs
@@ -2,6 +2,7 @@ using Amazon;
 using Amazon.DynamoDBv2;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
 using RaspberryPi.API.Configuration;
 using RaspberryPi.API.Contracts.Options;
 using RaspberryPi.API.Repositories;
@@ -10,13 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
 var jsonOptions = AppJsonSerializerOptions.Default;
 
-//builder.Services.Configure<AwsOptions>(builder.Configuration.GetSection(AwsOptions.Aws));
-var awsOptions = config.GetSection(AwsOptions.Aws)
-                       .Get<AwsOptions>();
-
 // Add services to the container.
 builder.Services.AddHealthChecks();
-builder.Services.AddControllers();
 builder.Services.AddControllers()
                 .AddJsonOptions(options =>
                 {
@@ -27,8 +23,11 @@ builder.Services.AddControllers()
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var awsOptions = config.GetSection(AwsOptions.Aws)
+                       .Get<AwsOptions>();
+
 var dbClient = new AmazonDynamoDBClient(awsOptions.AccessKeyId, 
-                                        awsOptions.SecretAccessKey, 
+                                        awsOptions.SecretAccessKey,
                                         RegionEndpoint.USEast1);
 
 builder.Services.AddSingleton<IAmazonDynamoDB>(_ => dbClient);
@@ -36,24 +35,33 @@ builder.Services.AddSingleton<ICommentRepository, CommentRepository>();
 
 var app = builder.Build();
 
-app.UseDeveloperExceptionPage();
-app.UseSwagger();
-app.UseSwaggerUI();
-
 //if (app.Environment.IsDevelopment())
-//{
-//    app.UseDeveloperExceptionPage();
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+app.UseDeveloperExceptionPage();
+app.UseSwagger(x =>
+{
+    x.PreSerializeFilters.Add((swaggerDoc, httpRequest) =>
+    {
+        if (!httpRequest.Headers.ContainsKey("X-Forwarded-Host")) return;
+
+        var serverUrl = $"{httpRequest.Headers["X-Forwarded-Proto"]}://" +
+                        $"{httpRequest.Headers["X-Forwarded-Host"]}/" +
+                        $"{httpRequest.Headers["X-Forwarded-Prefix"]}";
+
+        swaggerDoc.Servers = new List<OpenApiServer>()
+        {
+            new OpenApiServer { Url = serverUrl }
+        };
+    });
+});
+app.UseSwaggerUI(x =>
+{
+    x.EnableTryItOutByDefault();
+});
 
 app.MapHealthChecks("/health", new HealthCheckOptions
 {
     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
 });
-
-//app.UseHttpsRedirection();
-//app.UseAuthorization();
 
 app.MapControllers();
 
