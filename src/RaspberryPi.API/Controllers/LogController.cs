e7b194a4a59d5dd3fe33d007fc3299eba8aaf238
Bruno Paes <bruno.campiol@gmail.com>
2023-09-13 20:41:10 -0300

    add log controller

---
+++ src/RaspberryPi.API/Controllers/LogController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RaspberryPi.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LogController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public LogController(ILogger<LogController> logger)
+        {
+                _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet("info/{message}")]
+        public async Task<IActionResult> Info(string message)
+        {
+            _logger.LogInformation(message);
+
+            return NoContent();
+        }
+
+        [HttpGet("warn/{message}")]
+        public async Task<IActionResult> Warn(string message)
+        {
+            _logger.LogWarning(message);
+
+            return NoContent();
+        }
+
+        [HttpGet("error/{message}")]
+        public async Task<IActionResult> Error(string message)
+        {
+            _logger.LogError(message);
+
+            return NoContent();
+        }
+    }
+}
