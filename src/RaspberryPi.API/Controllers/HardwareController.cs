2f3a0ded5afc7b021ddc579623be706719b67adf
Bruno Paes <bruno.paes@adp.com>
2023-10-17 17:34:40 -0300

    update harware controller

--- src/RaspberryPi.API/Controllers/HardwareController.cs
+++ src/RaspberryPi.API/Controllers/HardwareController.cs
@@ -8,6 +8,9 @@ namespace RaspberryPi.API.Controllers
     [Route("[controller]/[action]")]
     public class HardwareController : ControllerBase
     {
+        // TODO: update to libre hardware monitor
+        // https://github.com/LibreHardwareMonitor/LibreHardwareMonitor
+
         public HardwareController()
         {
 
@@ -39,77 +42,101 @@ namespace RaspberryPi.API.Controllers
             return Ok(sb.ToString());
         }
 
-        // TODO fix temperature
-        [HttpGet("temperature")]
-        public IActionResult Cpu()
-        {
-            var sb = new StringBuilder();
-
-            var computer = new Computer()
-            {
-                CPUEnabled = true
-            };
-            computer.Open();
-
-            foreach (var hardwareItem in computer.Hardware)
-            {
-                if (hardwareItem.HardwareType == HardwareType.CPU)
-                {
-                    sb.Append($"CPU {hardwareItem.Name}");
-                    sb.Append(Environment.NewLine);
-
-                    hardwareItem.Update();
-                    foreach (var sensor in hardwareItem.Sensors)
-                    {
-                        if (sensor.SensorType == SensorType.Temperature)
-                        {
-                            sb.Append($"CPU ({sensor.Index}) Temperature: {sensor.Value} °C");
-                            sb.Append(Environment.NewLine);
-                        }
-                    }
-                }
-            }
-
-            computer.Close();
-
-            return Ok(sb.ToString());
-        }
-
-        // TODO fix gpu temperature
         [HttpGet]
-        public IActionResult GpuTemp()
+        public IActionResult Report()
         {
-            var sb = new StringBuilder();
-
             var computer = new Computer()
             {
-                GPUEnabled = true
+                CPUEnabled = true,
+                HDDEnabled = true,
+                GPUEnabled = true,
+                FanControllerEnabled = true,
+                NICEnabled = true,
+                RAMEnabled = true,
+                MainboardEnabled = true,
             };
             computer.Open();
 
-            foreach (var hardwareItem in computer.Hardware)
-            {
-
-                if (hardwareItem.HardwareType == HardwareType.GpuNvidia)
-                {
-                    sb.Append($"CPU {hardwareItem.Name}");
-                    sb.Append(Environment.NewLine);
-
-                    hardwareItem.Update();
-                    foreach (var sensor in hardwareItem.Sensors)
-                    {
-                        if (sensor.SensorType == SensorType.Temperature)
-                        {
-                            sb.Append($"CPU ({sensor.Index}) Temperature: {sensor.Value} °C");
-                            sb.Append(Environment.NewLine);
-                        }
-                    }
-                }
-            }
-
+            var report = computer.GetReport();
             computer.Close();
-
-            return Ok(sb.ToString());
+            return Ok(report);
         }
+
+        //[HttpGet("temperature")]
+        //public IActionResult Cpu()
+        //{
+        //    var sb = new StringBuilder();
+
+        //    var computer = new Computer()
+        //    {
+        //        CPUEnabled = true
+        //    };
+        //    computer.Open();
+        //    var report = computer.GetReport();
+
+        //    foreach (var hardwareItem in computer.Hardware)
+        //    {
+        //        if (hardwareItem.HardwareType == HardwareType.CPU)
+        //        {
+        //            sb.Append($"CPU {hardwareItem.Name}");
+        //            sb.Append(Environment.NewLine);
+
+        //            hardwareItem.Update();
+        //            foreach (var sensor in hardwareItem.Sensors)
+        //            {
+        //                if (sensor.SensorType == SensorType.Temperature)
+        //                {
+        //                    sb.Append($"CPU ({sensor.Index}) Temperature: {sensor.Value} °C");
+        //                    sb.Append(Environment.NewLine);
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    computer.Close();
+
+        //    return Ok(sb.ToString());
+        //}
+
+        //[HttpGet("temperature")]
+        //public IActionResult Gpu()
+        //{
+        //    var sb = new StringBuilder();
+
+        //    var computer = new Computer()
+        //    {
+        //        GPUEnabled = true
+        //    };
+        //    computer.Open();
+
+        //    if (computer.Hardware.Length == 0)
+        //    {
+        //        sb.Append("There are no GPUs to display data");
+        //    }
+
+        //    foreach (var hardwareItem in computer.Hardware)
+        //    {
+        //        if (hardwareItem.HardwareType == HardwareType.GpuNvidia ||
+        //            hardwareItem.HardwareType == HardwareType.GpuAti)
+        //        {
+        //            sb.Append($"GPU {hardwareItem.Name}");
+        //            sb.Append(Environment.NewLine);
+
+        //            hardwareItem.Update();
+        //            foreach (var sensor in hardwareItem.Sensors)
+        //            {
+        //                if (sensor.SensorType == SensorType.Temperature)
+        //                {
+        //                    sb.Append($"GPU ({sensor.Index}) Temperature: {sensor.Value} °C");
+        //                    sb.Append(Environment.NewLine);
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    computer.Close();
+
+        //    return Ok(sb.ToString());
+        //}
     }
 }
