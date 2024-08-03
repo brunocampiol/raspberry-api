d749bcb5d1ab3d425ed56faf3b587bfc2a88103e
bruno <bruno.campiol@gmail.com>
2024-08-02 22:44:20 -0300

    removed comment

--- src/RaspberryPi.Domain/Core/IRepository.cs
+++ src/RaspberryPi.Domain/Core/IRepository.cs
@@ -2,7 +2,6 @@
 {
     public interface IRepository<T> : IDisposable where T : class
     {
-        //IUnitOfWork UnitOfWork { get; }
         Task<T?> GetByIdAsync(Guid id);
         Task AddAsync(T entity);
         void Update(T entity);
