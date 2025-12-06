79e5bc6ad47bfaf75ce4f7516a82f2c42e1542d9
bruno <1672440+brunocampiol@users.noreply.github.com>
2025-12-05 23:31:08 -0300

    fix warnings

--- src/RaspberryPi.API/Helpers/MethodTimeLogger.cs
+++ src/RaspberryPi.API/Helpers/MethodTimeLogger.cs
@@ -4,7 +4,13 @@ namespace RaspberryPi.API.Helpers;
 
 public static class MethodTimeLogger
 {
-    public static ILogger Logger;
+    private static ILogger? _logger;
+
+    public static ILogger Logger
+    {
+        get => _logger ?? throw new InvalidOperationException("Logger has not been initialized.");
+        set => _logger = value ?? throw new ArgumentNullException(nameof(value));
+    }
 
     public static void Log(MethodBase methodBase, TimeSpan timeSpan)
     {
