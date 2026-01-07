ac772f2dff0d462d010c159a325bf12854b46eed
bruno <1672440+brunocampiol@users.noreply.github.com>
2026-01-06 23:28:54 -0300

    add fact controller method summaries

--- src/RaspberryPi.API/Controllers/FactController.cs
+++ src/RaspberryPi.API/Controllers/FactController.cs
@@ -16,6 +16,10 @@ public class FactController : ControllerBase
         _service = service;
     }
 
+    /// <summary>
+    /// Retrieves all database facts.
+    /// </summary>
+    /// <returns></returns>
     [Time]
     [HttpGet]
     public async Task<IEnumerable<Fact>> All()
@@ -24,6 +28,10 @@ public class FactController : ControllerBase
         return result;
     }
 
+    /// <summary>
+    /// Retrieves the first available fact from the database, if any.
+    /// </summary>
+    /// <returns></returns>
     [Time]
     [HttpGet]
     public async Task<Fact?> FirstOrDefault()
@@ -32,9 +40,13 @@ public class FactController : ControllerBase
         return result;
     }
 
+    /// <summary>
+    /// Gets the total number of facts available in the data store.
+    /// </summary>
+    /// <returns></returns>
     [Time]
     [HttpGet]
-    public async Task<long> CountAll()
+    public async Task<long> TotalCount()
     {
         var result = await _service.CountAllFactsAsync();
         return result;
